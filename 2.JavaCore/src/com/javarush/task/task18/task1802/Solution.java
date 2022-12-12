6100e9bcf561bc0a0a41dd0bb0bd2cfd7bb30942
Azoniques <Azoniques@gmail.com>
2022-12-12 19:13:05 +0300

    1802 - done

--- 2.JavaCore/src/com/javarush/task/task18/task1802/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1802/Solution.java
@@ -10,5 +10,17 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName  = bufferedReader.readLine();
+
+        int min = Integer.MAX_VALUE;
+
+        try(FileInputStream fileInputStream = new FileInputStream(fileName)){
+            while (fileInputStream.available() >0){
+                int cur = fileInputStream.read();
+                if (cur<min) min = cur;
+            }
+        }
+        System.out.println( min);
     }
 }
