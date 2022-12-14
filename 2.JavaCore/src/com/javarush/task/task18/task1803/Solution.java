ab37a4b1e1757459f477566d58fd0ac8a8476c3f
Azoniques <Azoniques@gmail.com>
2022-12-14 18:30:33 +0300

    1803 - done

--- 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -11,5 +11,26 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+        long[] arrBytes = new long[256];
+        try (FileInputStream fis = new FileInputStream(
+                new BufferedReader(
+                        new InputStreamReader(System.in)).readLine())) {
+
+            while (fis.available() > 0) arrBytes[fis.read()]++;
+
+
+        }
+
+
+        long maxRepeat = 0;
+        for (int i = 0; i < arrBytes.length; i++) {
+            if (arrBytes[i] > maxRepeat) maxRepeat = arrBytes[i];
+        }
+        for (int i = 0; i < arrBytes.length; i++) {
+            if (arrBytes[i] == maxRepeat) System.out.print(i + " ");
+        }
+
+
     }
 }
