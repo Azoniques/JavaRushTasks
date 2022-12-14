14d82a20c75a19656848260c70c17af1182b2993
Azoniques <Azoniques@gmail.com>
2022-12-14 21:30:09 +0300

    1804 - done

--- 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -11,5 +11,22 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+        int[] array = new int[256];
+        try (FileInputStream fis = new FileInputStream(
+                new BufferedReader(
+                        new InputStreamReader(System.in)).readLine())) {
+            while (fis.available() > 0) array[fis.read()]++;
+        }
+        int minMatch = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0 && array[i] < minMatch) minMatch = array[i];
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == minMatch) System.out.print(i + " ");
+
+        }
+
+
     }
 }
