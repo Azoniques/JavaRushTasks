f68de0510b3479cc2d6857d166cbc867459631b4
Azoniques <Azoniques@gmail.com>
2022-12-14 22:16:31 +0300

    1805 - done

--- 2.JavaCore/src/com/javarush/task/task18/task1805/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1805/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task18.task1805;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.Set;
@@ -12,5 +13,28 @@ import java.util.TreeSet;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        long[] arrByte = new long[256];
+        byte[] buffer = new byte[64000];
+        int bufferSize = 64000;
+
+
+        try (FileInputStream fileInputStream = new FileInputStream(
+                new BufferedReader(
+                        new InputStreamReader(System.in)).readLine())) {
+            while (fileInputStream.available() > 0){
+                if (fileInputStream.available() < 64000) {
+                    bufferSize = fileInputStream.available();
+                }
+                fileInputStream.read(buffer, 0, bufferSize);
+            }
+            for (int i = 0; i < bufferSize; i++)
+                arrByte[buffer[i]]++;
+
+            for (int i = 0; i < arrByte.length; i++) {
+                if (arrByte[i] > 0) System.out.print(i + " ");
+            }
+        }
     }
+
 }
+
