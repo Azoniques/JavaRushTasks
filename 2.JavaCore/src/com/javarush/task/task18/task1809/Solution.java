79f613fcd3d6bf4dfa5ed8091f6e42a14c7fb7fb
Azoniques <Azoniques@gmail.com>
2022-12-16 12:15:32 +0300

    1809

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1809/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task18.task1809;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/* 
+Реверс файла
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String src = bufferedReader.readLine();
+        String dst = bufferedReader.readLine();
+
+        
+
+
+    }
+}
