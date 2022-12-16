4c9ef4845a8cda11620316a99451b4f99926db74
Azoniques <Azoniques@gmail.com>
2022-12-16 10:17:08 +0300

    1808

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task18.task1808;
+
+import java.io.*;
+
+/* 
+Разделение файла
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String inputFile = bufferedReader.readLine();
+        String outputFile1 = bufferedReader.readLine();
+        String outputFile2 = bufferedReader.readLine();
+
+
+
+
+    }
+}
