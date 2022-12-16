f5270e47aaca1ad72abd44b98ea07928e8d2892f
Azoniques <Azoniques@gmail.com>
2022-12-16 08:53:25 +0300

    1807-done

--- 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
@@ -1,15 +1,22 @@
 package com.javarush.task.task18.task1807;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /* 
 Подсчет запятых
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        int comma = (int)',';
+        int count=0;
+        try (FileInputStream fis = new FileInputStream(
+                new BufferedReader(
+                        new InputStreamReader(System.in)).readLine())){
+            while(fis.available()>0){
+                if(fis.read() == comma)count++;
+            }
+        };
+        System.out.println(count);
     }
 }
