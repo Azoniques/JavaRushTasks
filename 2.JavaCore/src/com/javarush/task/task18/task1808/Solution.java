cc5594e71dd2981a8a3451c6ff6cfe72feff88c7
Azoniques <Azoniques@gmail.com>
2022-12-16 11:32:50 +0300

    1808 - done

--- 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
@@ -13,8 +13,23 @@ public class Solution {
         String outputFile1 = bufferedReader.readLine();
         String outputFile2 = bufferedReader.readLine();
 
+        try(FileInputStream fis = new FileInputStream(inputFile);
+            FileOutputStream fos1 = new FileOutputStream(outputFile1);
+            FileOutputStream fos2 = new FileOutputStream(outputFile2)){
 
+            byte [] buffer = new byte[fis.available()];
+            fis.read(buffer,0, buffer.length);
+            fos1.write(buffer, 0, buffer.length/2+ buffer.length%2);
+            fos2.write(buffer, buffer.length/2+ buffer.length%2, buffer.length/2);
+
+
+            }
+
+
+
+
+        }
 
 
     }
-}
+
