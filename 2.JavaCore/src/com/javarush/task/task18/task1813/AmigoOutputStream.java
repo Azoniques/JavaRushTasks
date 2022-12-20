2244c6446395299a2d15162b61efc62d32ee972e
Azoniques <Azoniques@gmail.com>
2022-12-20 14:50:41 +0300

    1813 - done

--- 2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task18.task1813;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -8,11 +9,36 @@ import java.io.IOException;
 AmigoOutputStream
 */
 
-public class AmigoOutputStream {
+public class AmigoOutputStream extends FileOutputStream {
     public static String fileName = "C:/tmp/result.txt";
-
+    FileOutputStream fileOutputStream;
+    public AmigoOutputStream(FileOutputStream wrapee) throws FileNotFoundException {
+        super(fileName);
+        this.fileOutputStream = wrapee;
+    }
     public static void main(String[] args) throws FileNotFoundException {
         new AmigoOutputStream(new FileOutputStream(fileName));
     }
-
+    @Override
+    public void write(int b) throws IOException {
+        fileOutputStream.write(b);
+    }
+    @Override
+    public void write(byte[] b) throws IOException {
+        fileOutputStream.write(b);
+    }
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        fileOutputStream.write(b, off, len);
+    }
+    @Override
+    public void flush() throws IOException {
+        fileOutputStream.flush();
+    }
+    @Override
+    public void close() throws IOException {
+        flush();
+        write("JavaRush © All rights reserved.".getBytes());
+        fileOutputStream.close();
+    }
 }
