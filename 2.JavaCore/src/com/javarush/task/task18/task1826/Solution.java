70daf568b42d0736035707f548e65c9b79e6163b
”Azoniques” <Azoniques@gmail.com>
2023-01-24 21:58:04 +0300

    1826 - done

--- 2.JavaCore/src/com/javarush/task/task18/task1826/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1826/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task18.task1826;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -9,8 +10,25 @@ import java.io.IOException;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String mode = args[0];
+        if (!(args[0].equals("-d") || args[0].equals("-e"))) return;
+        try (FileInputStream fileInputStream = new FileInputStream(args[1]);
+             FileOutputStream fileOutputStream = new FileOutputStream(args[2])) {
+            if (mode.equals("-e")) {
+                while (fileInputStream.available() > 0) {
+                    fileOutputStream.write(fileInputStream.read() + 1);
+                }
+            } else {
+                while (fileInputStream.available() > 0) {
+                    fileOutputStream.write(fileInputStream.read() - 1);
+                }
+
+
+            }
+
+
+        }
 
     }
-
 }
