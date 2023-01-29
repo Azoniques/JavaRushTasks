b02949c7dc15970918e13c3872b1323024bd77c1
”Azoniques” <Azoniques@gmail.com>
2023-01-29 22:51:02 +0300

    1817 - done

--- 2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
@@ -9,7 +9,17 @@ import java.io.IOException;
 */
 
 public class Solution {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
+//        args = new String[]{"D:\\test.txt"};
+        int countSpace = 0;
+        int countChar = 0;
+        try (FileReader fileReader = new FileReader(args[0])) {
+            while (fileReader.ready()){
+                countChar++;
+                if (' ' == (char)fileReader.read()) countSpace++;
+            }
+        }
+        System.out.format("%.2f", 1.0*countSpace/countChar*100);
 
         }
 
