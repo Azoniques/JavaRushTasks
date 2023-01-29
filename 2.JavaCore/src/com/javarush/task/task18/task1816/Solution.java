d0ffffd7d2c25b66371c24e8f2e960029e670a04
”Azoniques” <Azoniques@gmail.com>
2023-01-29 22:22:41 +0300

    1816 - done

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task18.task1816;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Английские буквы
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //args[0] = "D:\\test.txt";
+        int counter = 0;
+        FileInputStream fileInputStream= new  FileInputStream(args[0]);
+        while(fileInputStream.available()>0) {
+            byte b = (byte) fileInputStream.read();
+            if (b >= 'A' & b <= 'Z' || b >= 'a' & b <= 'z') counter++;
+
+
+        }
+        System.out.println(counter);
+        }
+    }
+
