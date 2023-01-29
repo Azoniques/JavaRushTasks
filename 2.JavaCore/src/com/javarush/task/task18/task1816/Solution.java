4ae021f09666f117a4852e34686458a2f8cc0f79
”Azoniques” <Azoniques@gmail.com>
2023-01-29 22:24:35 +0300

    1826 - done - add TryWithResourses

--- 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
@@ -13,12 +13,13 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         //args[0] = "D:\\test.txt";
         int counter = 0;
-        FileInputStream fileInputStream= new  FileInputStream(args[0]);
-        while(fileInputStream.available()>0) {
-            byte b = (byte) fileInputStream.read();
-            if (b >= 'A' & b <= 'Z' || b >= 'a' & b <= 'z') counter++;
+        try (FileInputStream fileInputStream = new FileInputStream(args[0])) {
+            while (fileInputStream.available() > 0) {
+                byte b = (byte) fileInputStream.read();
+                if (b >= 'A' & b <= 'Z' || b >= 'a' & b <= 'z') counter++;
 
 
+            }
         }
         System.out.println(counter);
         }
