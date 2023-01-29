f7b4b364b0ee34f4cedc3786e34d2f060274f4f6
”Azoniques” <Azoniques@gmail.com>
2023-01-29 22:41:01 +0300

    1817

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task18.task1817;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/* 
+Пробелы
+*/
+
+public class Solution {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        }
+
+    }
