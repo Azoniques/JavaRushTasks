533b4045d47b2e5c66cc55c1ff2c14adee63a227
”Azoniques” <Azoniques@gmail.com>
2023-01-30 22:12:53 +0300

    1821-done

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task18.task1821;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -8,7 +10,22 @@ import java.io.IOException;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+//        args[0] = "D:\\test2.txt";
+        FileInputStream fileInputStream = new FileInputStream(args[0]);
 
+        int[] array = new int[256];
+            while(fileInputStream.available()>0){
+                int i = fileInputStream.read();
+                array[i] ++;
+            }
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] !=0){
+                System.out.println((char) i+ " "+ array[i]);
+            }
+
+
+        }
+//        System.out.println(array);
     }
 }
