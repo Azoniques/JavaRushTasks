1e577b0d76801f7658b2758bc097d0953761a799
”Azoniques” <Azoniques@gmail.com>
2023-01-31 21:48:41 +0300

    1823 - done

--- 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.task.task18.task1823;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,14 +11,39 @@ import java.util.Map;
 public class Solution {
     public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String readString;
+        while (!("exit".equals(readString = reader.readLine()))){
+            new ReadThread(readString).start();
+        }
     }
 
     public static class ReadThread extends Thread {
+        String fileName;
+        
         public ReadThread(String fileName) {
-            //implement constructor body
+            this.fileName = fileName;
         }
-        // implement file reading here - реализуйте чтение из файла тут
+        public void run(){
+            byte [] bytes = new byte[256];
+            try (FileInputStream fileInputStream = new FileInputStream(fileName)){
+                while (fileInputStream.available()>0){
+                    int aByte = fileInputStream.read();
+                    bytes[aByte] ++;
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            int maxCount = 0;
+            int targetByte = 0;
+            for (int i = 0; i < bytes.length; i++) {
+                if (bytes[i]> maxCount){
+                    maxCount = bytes[i];
+                    targetByte= i;
+                }
+            }
+            resultMap.put(fileName, targetByte);
+        }
     }
 }
