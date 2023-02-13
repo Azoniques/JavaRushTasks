eedc803720acb42af1ff19f28f89f3c8d85b5da1
Azoniques <Azoniques@gmail.com>
2023-02-13 22:30:22 +0300

    1910

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1910/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task19.task1910;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/* 
+Пунктуация
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        String inputFile;
+        String outputFile;
+        try(BufferedReader console = new BufferedReader(new InputStreamReader(System.in))){
+            inputFile = console.readLine();
+            outputFile = console.readLine();
+        }
+        ArrayList<String> content = new ArrayList<>();
+
+        try(BufferedReader reader )
+
+
+
+
+
+
+    }
+}
