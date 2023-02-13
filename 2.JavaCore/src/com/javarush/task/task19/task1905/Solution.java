9e89cd8a495c90849a35922c618b278ce3991a68
Azoniques <Azoniques@gmail.com>
2023-02-13 19:59:25 +0300

    1905 - done

--- 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
@@ -9,7 +9,8 @@ import java.util.Map;
 
 public class Solution {
     public static Map<String, String> countries = new HashMap<String, String>();
-    static{
+
+    static {
         countries.put("UA", "Ukraine");
         countries.put("RU", "Russia");
         countries.put("CA", "Canada");
@@ -19,8 +20,48 @@ public class Solution {
 
     }
 
-    public static class DataAdapter {
+    public static class DataAdapter implements RowItem {
+
+        private Customer customer;
+        private Contact contact;
+
         public DataAdapter(Customer customer, Contact contact) {
+            this.contact = contact;
+            this.customer = customer;
+
+        }
+
+        @Override
+        public String getCountryCode() {
+            for (Map.Entry<String, String> entry : countries.entrySet()) {
+                if (entry.getValue().equals(customer.getCountryName())) {
+                    return entry.getKey();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public String getCompany() {
+            return customer.getCompanyName();
+        }
+
+        @Override
+        public String getContactFirstName() {
+            String[] name = contact.getName().split(", ");
+            return name[1];
+        }
+
+        @Override
+        public String getContactLastName() {
+            String[] name = contact.getName().split(", ");
+            return name[0];
+        }
+
+        @Override
+        public String getDialString() {
+
+            return "callto://+" + contact.getPhoneNumber().replaceAll("\\D", "");
         }
     }
 
