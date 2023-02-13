8c34c2c6fdd29327969bb2f9f89f37a276ba94f6
Azoniques <Azoniques@gmail.com>
2023-02-13 23:18:01 +0300

    1908 - done

--- 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
@@ -8,6 +8,39 @@ import java.util.ArrayList;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String inputFile;
+        String outputFile;
+
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
+            inputFile = console.readLine();
+            outputFile = console.readLine();
+        }
+
+        ArrayList<String> content = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+            while (reader.ready()) {
+                content.add(reader.readLine());
+            }
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+
+            for (String line : content) {
+                String[] words = line.trim().split(" ");
+                for (String word : words) {
+                    try {
+                        int d = Integer.parseInt(word);
+                        writer.write(word + " ");
+                    } catch (Exception ignore) {
+                    }
+
+                }
+            }
+
+        }
     }
+
 }
+
