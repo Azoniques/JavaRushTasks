afe8384d8c1f0eacb7bd146f8dbf7d4fca071b68
Azoniques <Azoniques@gmail.com>
2023-02-13 22:43:30 +0300

    1910 - punctuation

--- 2.JavaCore/src/com/javarush/task/task19/task1910/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1910/Solution.java
@@ -11,28 +11,24 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         String inputFile;
         String outputFile;
-        try(BufferedReader console = new BufferedReader(new InputStreamReader(System.in))){
+
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
             inputFile = console.readLine();
             outputFile = console.readLine();
         }
+
         ArrayList<String> content = new ArrayList<>();
 
-        try(BufferedReader reader = new BufferedReader(new FileReader(inputFile))){
-            while (reader.ready()){
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+            while (reader.ready()) {
                 content.add(reader.readLine());
             }
         }
 
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))){
-            for (String s : content){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+            for (String s : content) {
                 writer.write(s.replaceAll("\\p{Punct}", ""));
             }
         }
-
-
-
-
-
-
     }
 }
