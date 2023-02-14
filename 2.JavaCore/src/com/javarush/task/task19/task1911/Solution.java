68219707ddbbc821eb914582f85ac2e2bbceb266
Azoniques <Azoniques@gmail.com>
2023-02-14 23:03:08 +0300

    1911 - done

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1911/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task19.task1911;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Ридер обертка
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream out = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(byteArrayOutputStream);
+        System.setOut(ps);
+        testString.printSomething();
+        String result = byteArrayOutputStream.toString().toUpperCase();
+        System.setOut(out);
+        System.out.println(result);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
