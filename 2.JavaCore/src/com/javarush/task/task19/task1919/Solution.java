c4a1d7b244af2ea266f402e7326e46bff591decb
Azoniques <Azoniques@gmail.com>
2023-02-19 22:42:41 +0300

    1919 - done

--- 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task19.task1919;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
 import java.util.TreeMap;
 
 /* 
@@ -10,6 +12,24 @@ import java.util.TreeMap;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String fileName = args[0];
+        Map<String, Double> salary = new TreeMap<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            while(reader.ready()){
+                String[] buffer = reader.readLine().split(" ");
+                String key = buffer[0];
+                double d = Double.parseDouble(buffer[1]);
+                salary.merge(key, d, Double::sum);
+//                salary.put(key, salary.getOrDefault(key, 0.0)+ d);
+            }
+
+        }
+        salary.forEach((k,v)-> System.out.println(k+" "+v));
+
+
+
+
+
     }
 }
