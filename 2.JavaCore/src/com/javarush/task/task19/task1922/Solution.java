3976636b6f0012d34501473bc6f852626bacf6dd
Azoniques <Azoniques@gmail.com>
2023-02-28 21:37:52 +0300

    1922

--- 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /* 
 Ищем нужные строки
@@ -20,7 +22,24 @@ public class Solution {
         words.add("В");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String fileName = null;
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(System.in))) {
+            fileName = input.readLine();
+            }
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            while((line = reader.readLine()) != null){
+                int count = 0;
+                for(String word:words){
+                    Pattern p = Pattern.compile("\\b" + word + "\\b");
+                    Matcher m = p.matcher(line);
+                    if (m.find()) count++;
+                }
+                if(count == 2) System.out.println(line);
+            }
 
+
+        }
     }
 }
