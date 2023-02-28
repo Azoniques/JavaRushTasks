185691d5cd814b3114a5c9681525ccd03c9b58e6
Azoniques <Azoniques@gmail.com>
2023-02-28 22:17:08 +0300

    1923 - done

--- 2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.task.task19.task1923;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 /* 
 Слова с цифрами
@@ -11,5 +8,29 @@ import java.io.IOException;
 
 public class Solution {
     public static void main(String[] args) {
+//        String fileName1 = args[0];
+//        String fileName2 = args[1];
+
+        String fileName1 = "D:\\Test.txt";
+        String fileName2 = "D:\\1Test.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName1));
+            FileWriter fileWriter = new FileWriter(fileName2)){
+            String line;
+            String[] strings;
+            while ((line = reader.readLine()) != null) {
+                strings = line.split(" ");
+                for(String word: strings){
+                    if(word.matches(".*[0-9].*")){
+                        fileWriter.write(word + " ");
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
-}
+    }
