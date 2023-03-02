d5c69fb076b5fd6271e2a2c034e300797732da74
azoniques <Azoniques@gmail.com>
2023-03-02 22:26:24 +0300

    1926 - done

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1926/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task19.task1926;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Перевертыши
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        String fileName;
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
+            fileName = console.readLine();
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            reader.lines()
+                    .map(s->new StringBuilder().append(s).reverse())
+                    .forEach(System.out::println);
+
+//            while(reader.ready()){
+//                System.out.println(new StringBuilder(reader.readLine()).reverse());
+//            }
+        }
+    }
+}
