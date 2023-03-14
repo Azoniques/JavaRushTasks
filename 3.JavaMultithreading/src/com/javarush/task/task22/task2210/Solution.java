2e4aa29ee7f86493f89095d2136dfdeb7da513c4
azoniques <Azoniques@gmail.com>
2023-03-14 16:11:01 +0300

    2210 - done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task22.task2210;
+
+import java.util.StringTokenizer;
+
+/* 
+StringTokenizer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+
+        StringTokenizer tokenizer = new StringTokenizer(query, delimiter);
+        String [] res = new String [tokenizer.countTokens()];
+        int i =0;
+        while (tokenizer.hasMoreTokens()){
+
+            String token = tokenizer.nextToken();
+            res[i++] =token;
+        }
+
+
+
+
+
+        return res;
+    }
+}
