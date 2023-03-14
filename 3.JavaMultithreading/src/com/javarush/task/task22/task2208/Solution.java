0b5be3f2cfc65edacfb3d094c18d740d59a701b2
azoniques <Azoniques@gmail.com>
2023-03-14 15:24:30 +0300

    2208 - done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task22.task2208;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/* 
+Формируем WHERE
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static String getQuery(Map<String, String> params) {
+        StringBuilder queryBuilder = new StringBuilder();
+        for (String s : params.keySet()) {
+            String value = params.get(s);
+            if (value == null) {
+                continue;
+            }
+            if (queryBuilder.toString().length() != 0) {
+                queryBuilder.append(" and ");
+            }
+            queryBuilder.append(s + " = '" + value + "'");
+        }
+        return queryBuilder.toString();
+    }
+}
+
