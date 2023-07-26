c59a6eb19b60e5c2684d035399bf4909ded7a9cc
azoniques <Azoniques@gmail.com>
2023-07-26 22:10:54 +0300

    2515 - (5)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
@@ -21,6 +21,17 @@ public class Space {
     public static void main(String... args) {
     }
 
+    public void run(){
+
+    }
+
+    public void draw(){
+
+    }
+
+    public void sleep(int ms){
+        
+    }
     public void setShip(SpaceShip ship) {
         this.ship = ship;
     }
