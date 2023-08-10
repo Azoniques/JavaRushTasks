4ca15a416a734feae8e733666db3c0fce5717e43
azoniques <Azoniques@gmail.com>
2023-08-11 00:48:35 +0300

    LOG- DONE 2

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3401/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task34.task3401;
+
+/* 
+Числа Фибоначчи с помощью рекурсии
+*/
+
+import java.time.Duration;
+import java.time.Instant;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        Instant start = Instant.now();
+        System.out.println(solution.fibonacci(45));//34
+        Instant finish = Instant.now();
+        /*System.out.println(solution.fibonacci(5));     //5
+
+        System.out.println(solution.fibonacci(2));     //1
+        System.out.println(solution.fibonacci(1));     //1*/
+        long elapsed = Duration.between(start, finish).toMillisPart();
+        System.out.println("calculating time: " + elapsed);
+    }
+
+    public int fibonacci(int n) {
+        if (n==1 || n==2) return 1;
+        else return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
